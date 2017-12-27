51f437419adf45888c89532dcc0fa64175f3540e
Sasa Bojanic <s.bojanic@togetherteam.co.th>
2017-12-27 12:43:12 +0100

    Setting different tenant id and app id

--- scripts/auth.js
+++ scripts/auth.js
@@ -7,8 +7,8 @@ var mailboxSettingsAvailable = true;
 };*/
 var ADAL = new AuthenticationContext({
     instance: 'https://login.microsoftonline.com/',
-    tenant: 'a52ca09a-09ef-4eaf-9a51-6a68b32acf5c', //COMMON OR YOUR TENANT ID
-    clientId: '5f3466b8-07aa-4c1b-a30f-b81e13c1480c', //REPLACE WITH YOUR CLIENT ID
+    tenant: 'b4a7cf6c-8876-456a-b97f-1e2bbeb7579a', //COMMON OR YOUR TENANT ID
+    clientId: '0ca2fffb-575d-4af8-93b3-aa148585f60b', //REPLACE WITH YOUR CLIENT ID
     redirectUri: [window.location.protocol, '//', window.location.host, window.location.pathname].join(''), // THE CDN URI
     cacheLocation: isIEBrowser() ? 'localStorage' : 'sessionStorage', // enable this for IE, as sessionStorage does not work for localhost.
     //endpoints: endpoints,
